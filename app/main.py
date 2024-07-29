efe73a57d7e6e0127542c2c35772235419401fdc
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-07-29 19:55:42 +0200

    update

--- app/main.py
+++ app/main.py
@@ -2,33 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .db import postgrestables
 from .routers import auth
+from .utils import SecurityHeadersMiddleware
 from .db.postgres import engine
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.responses import Response
 
 postgrestables.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-class SecurityHeadersMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request, call_next):
-        response = await call_next(request)
-        response.headers['Content-Security-Policy'] = "default-src 'self'"
-        response.headers['X-Content-Type-Options'] = 'nosniff'
-        response.headers['X-Frame-Options'] = 'DENY'
-        response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
-        response.headers['X-XSS-Protection'] = '1; mode=block'
-        return response
-
 app.add_middleware(SecurityHeadersMiddleware)
-
-origins = [
-    "http://localhost:3000",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],
--- app/utils.py
+++ app/utils.py
@@ -3,6 +3,19 @@ import time
 from .config import psql
 from psycopg2.extras import RealDictCursor
 from passlib.context import CryptContext
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
+from time import time
+
+class SecurityHeadersMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        response.headers['Content-Security-Policy'] = "default-src 'self'"
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        response.headers['X-Frame-Options'] = 'DENY'
+        response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+        response.headers['X-XSS-Protection'] = '1; mode=block'
+        return response
 
 pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
@@ -16,11 +29,11 @@ def verify(plain_password, hashed_password):
 def checkdb():
     while True:
         try:
-            psycopg2.connect(host=psql.host_psql, 
-                                    database=psql.db_psql, 
-                                    user=psql.user_psql,
-                                    password=psql.pw_psql, 
-                                    cursor_factory=RealDictCursor)
+            psycopg2.connect(host=psql.host_psql,
+                             database=psql.db_psql,
+                             user=psql.user_psql,
+                             password=psql.pw_psql,
+                             cursor_factory=RealDictCursor)
             print("Database connection was succesfull!")
             break
         except Exception as error:
