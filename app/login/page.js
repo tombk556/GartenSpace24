ef210b9290b2f699e9f7f5d54dbc09baa8af99b1
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-08-13 17:13:16 +0200

    update

--- app/login/page.js
+++ app/login/page.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Link from "next/link";
+
 const page = () => {
   return (
-    <div>
+    <div className="">
       Login Page
       <p>Dont have an account?</p>
       <Link href="/signup" className="black_btn">
