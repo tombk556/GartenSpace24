68eacd03b49abc39f50e2fb8a12a671687fba58a
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-08-13 17:01:30 +0200

    update

--- components/Nav.js
+++ components/Nav.js
@@ -2,11 +2,8 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
 
 const Nav = () => {
-  const [isSignedIn, setIsSignedIn] = useState(false);
-
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -19,39 +16,14 @@ const Nav = () => {
         />
       </Link>
       <div className="sm:flex hidden">
-        {isSignedIn ? (
-          <div className="flex gap-3 md:gap-5">
-            <Link href="/create-prompt" className="black_btn">
-              Create Post
-            </Link>
-
-            <button
-              type="button"
-              onClick={() => setIsSignedIn(false)} // Simulate sign out
-              className="outline_btn"
-            >
-              Sign Out
-            </button>
-
-            <Link href="/profile">
-              <Image
-                src="/assets/images/logo.svg"
-                width={37}
-                height={37}
-                className="rounded-full"
-                alt="profile"
-              />
-            </Link>
-          </div>
-        ) : (
-          <button
-            type="button"
-            onClick={() => setIsSignedIn(true)} // Simulate sign in
-            className="outline_btn"
-          >
-            Sign in
-          </button>
-        )}
+        <div className="flex gap-3 md:gap-5">
+          <Link href="/login" className="outline_btn">
+            Login
+          </Link>
+          <Link href="/signup" className="outline_btn">
+            Signup
+          </Link>
+        </div>
       </div>
     </nav>
   );
