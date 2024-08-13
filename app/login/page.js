727c3140578c8a4c711f7fbe5070431f8d84a32a
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-08-13 17:34:09 +0200

    added login

--- app/login/page.js
+++ app/login/page.js
@@ -1,16 +1,86 @@
-import React from "react";
+"use client";
 import Link from "next/link";
 
-const page = () => {
+export default function Page() {
   return (
-    <div className="">
-      Login Page
-      <p>Dont have an account?</p>
-      <Link href="/signup" className="black_btn">
-        Signup
-      </Link>
-    </div>
-  );
-};
+        <div className="grid md:grid-cols-2 items-center gap-10 max-w-6xl w-full">
+          <div>
+            <h2 className="lg:text-5xl text-4xl font-extrabold lg:leading-[55px] text-gray-800">
+              Seamless Login for Exclusive Access
+            </h2>
+            <p className="text-sm mt-6 text-gray-800">
+              Immerse yourself in a hassle-free login journey with our
+              intuitively designed login form. Effortlessly access your account.
+            </p>
+            <p className="text-sm mt-12 text-gray-800">
+              Don't have an account?{" "}
+              <Link href="/register" className="text-blue-600 font-semibold hover:underline">
+                  Register here
+              </Link>
+            </p>
+          </div>
 
-export default page;
+          <form className="max-w-md md:ml-auto w-full">
+            <h3 className="text-gray-800 text-3xl font-extrabold mb-8">
+              Sign in
+            </h3>
+
+            <div className="space-y-4">
+              <div>
+                <input
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="bg-gray-100 w-full text-sm text-gray-800 px-4 py-3.5 rounded-md outline-blue-600 focus:bg-transparent"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <input
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="bg-gray-100 w-full text-sm text-gray-800 px-4 py-3.5 rounded-md outline-blue-600 focus:bg-transparent"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="flex flex-wrap items-center justify-between gap-4">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                  />
+                  <label
+                    htmlFor="remember-me"
+                    className="ml-3 block text-sm text-gray-800"
+                  >
+                    Remember me
+                  </label>
+                </div>
+                <div className="text-sm">
+                  <Link
+                    href="/forgot-password"
+                    className="text-blue-600 hover:text-blue-500 font-semibold"
+                  >
+                    Forgot your password?
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-8">
+              <button
+                type="submit"
+                className="w-full shadow-xl py-2.5 px-4 text-sm font-semibold rounded text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
+              >
+                Log in
+              </button>
+            </div>
+          </form>
+        </div>
+  );
+}
