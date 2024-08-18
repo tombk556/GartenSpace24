1857d2d6d94da608748254d377523c52ca8f135f
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-08-18 15:45:17 +0200

    update

--- app/google/callback/page.js
+++ app/google/callback/page.js
@@ -1,17 +1,18 @@
 "use client";
 import { useEffect } from "react";
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
+  const router = useRouter();
 
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
     const token = queryParams.get("token");
-
     if (token) {
       localStorage.setItem("access_token", token);
-      console.log("token", token);
+      router.push('/dashboard')
     } else {
-        console.log("no token");
+      router.push('/login')
     }
   }, []);
 
