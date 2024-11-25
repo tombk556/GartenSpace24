19fc13c98946f7925c12e81957ac2240eaaf4a02
Tom Bischopink <s87622@informatik.htw-dresden.de>
2024-11-25 14:20:20 +0100

    update

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,37 +1,12 @@
-"use client";
-
-import React from "react";
-import Link from "next/link";
-import useEntities from "./useEntities";
-import EntityTable from "./EntityTable";
-
+import React from 'react'
+import ProtectedRoute from '@components/ProtectedRoute'
+import Profile from './Profile'
 const page = () => {
-  const { entities, loading, error, deleteEntity } = useEntities();
-
-  if (loading) {
-    return <div className="p-4">Lädt...</div>;
-  }
-
-  if (error) {
-    return <div className="p-4 text-red-600">{error}</div>;
-  }
-
   return (
-    <div className="p-4">
-      <p className="text-xl orange_text font-bold mb-4">Meine Anzeigen</p>
+    <ProtectedRoute>
+      <Profile />
+    </ProtectedRoute>
+  )
+}
 
-      {entities.length === 0 ? (
-        <div className="text-gray-700">
-          Sie haben keine Anzeigen.{" "}
-          <Link href="/publishentity" className="orange_text hover:underline">
-            Jetzt Anzeige erstellen
-          </Link>
-        </div>
-      ) : (
-        <EntityTable entities={entities} onDelete={deleteEntity} />
-      )}
-    </div>
-  );
-};
-
-export default page;
+export default page
