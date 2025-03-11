67bb5c32db3b0a15449f7bc29a2f7277e4a9440c
Tom Bischopink <s87622@informatik.htw-dresden.de>
2025-03-11 17:35:59 +0100

    update searchbar

---
+++ components/SearchBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaAngleDown } from "react-icons/fa6";
+
+const SearchBar = () => {
+  return (
+<div className="mt-6 bg-white rounded-lg p-2 w-[800px] border-2 border-black">
+  <div className="flex flex-col md:flex-row items-center w-full">
+    <input
+      type="text"
+      placeholder="Ort, Stadt, Land oder PLZ eingeben"
+      className="p-2 w-full text-xl outline-none border-r-2 border-black"
+    />
+    <div className="relative w-full md:w-1/3">
+      <select className="appearance-none rounded-none p-3 w-full bg-white outline-none text-xl ">
+        <option value="Mieten">Mieten</option>
+        <option value="Kaufen">Kaufen</option>
+        <option value="Pachten">Pachten</option>
+      </select>
+      <FaAngleDown className="absolute inset-y-4 right-2 items-center" size={20} />
+    </div>
+    <div>
+        <button className="ml-2 bg-[#009243] text-white text-l px-4 py-2 rounded-2xl hover:bg-green-800">Suchen</button>
+    </div>
+  </div>
+</div>
+
+
+  );
+};
+
+export default SearchBar;
