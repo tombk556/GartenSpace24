452676004cbf94d064b846cf3347604d0484b2a1
Tom Bischopink <s87622@informatik.htw-dresden.de>
2025-03-17 09:49:55 +0100

    update

--- components/ErrorMessage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ErrorMessage = ({message}) => (
-    <p className="text-red-700"> {message}</p>
-)
-
-export default ErrorMessage;
--- components/SuccessMessage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SuccessMessage = ({message}) => (
-    <p className="text-green-700"> {message}</p>
-)
-
-export default SuccessMessage;
